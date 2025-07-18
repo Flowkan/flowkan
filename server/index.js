2377f1a8c9efd96ec17ab2a52473f53a91c5931f
Oscar <oscar.caasmontes@gmail.com>
2025-07-18 16:51:39 +0200

    Instalacion de prisma para migraciones y ORM de base de datos

--- server/index.js
+++ server/index.js
@@ -1,26 +1,25 @@
 import express from "express";
-import pkg from "pg";
-
-const { Pool } = pkg;
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
+  console.log("Esta versión del servidor está corriendo muy bien");
   res.send("Hola mundo desde ESM");
 });
 
 app.get("/db", async (req, res) => {
   try {
-    const result = await pool.query("SELECT NOW()");
-    res.json(result.rows[0]);
+    const boards = await prisma.board.findMany({
+      include: {
+        lists: true,
+        members: {
+          include: { user: true },
+        },
+      },
+    });
+    res.json(boards);
   } catch (err) {
     res.status(500).send("Error al conectar a la base de datos");
   }
