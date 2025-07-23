f78f6d359f05d643f9f18a0a5dc62301c93f9f00
Hector Lozano <hectorlg87@gmail.com>
2025-07-24 01:34:06 +0200

    feat: routing structure and creation of Home and Not Found pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Layout } from "./components/layout/layout";
+import { HomePage } from "./pages/home";
+import { NotFound } from "./pages/not-found";
+
 function App() {
-  return <h1>Hello Flowkan</h1>;
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<HomePage />} />
+
+				<Route path="not-found" element={<NotFound />} />
+				<Route path="*" element={<Navigate to="/not-found" />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
