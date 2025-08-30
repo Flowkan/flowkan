ea3562b81073ef5acec1e18f5d8ff3ecea9c8b6c
Hector Lozano <hectorlg87@gmail.com>
2025-08-30 15:35:19 +0200

    chore: rename type ValidateTypeSchema for  AuthScheme

--- server/src/validators/authValidator.ts
+++ server/src/validators/authValidator.ts
@@ -2,10 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import { loginSchema, registerSchema } from "./authSchema";
 import { ValidationError } from "./validationError";
 
-export type ValidateTypeSchema = typeof registerSchema | typeof loginSchema;
+export type AuthSchema = typeof registerSchema | typeof loginSchema;
 
 export const validateUserFields =
-  (schema: ValidateTypeSchema) => (req: Request, res: Response, next: NextFunction) => {
+  (schema: AuthSchema) => (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
     if (!result.success) {
       return next(new ValidationError(result.error));
