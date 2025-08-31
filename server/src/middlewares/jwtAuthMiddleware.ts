54c2691ae8fc537ab510a232f68161f6da488d8d
Hector Lozano <hectorlg87@gmail.com>
2025-08-31 17:05:25 +0200

    fix: interface JwtPayload type user_id and globals express.d.ts

--- server/src/middlewares/jwtAuthMiddleware.ts
+++ server/src/middlewares/jwtAuthMiddleware.ts
@@ -3,7 +3,7 @@ import createHttpError from "http-errors";
 import { NextFunction, Request, Response } from "express";
 
 export interface JwtPayload {
-  user_id: string;
+  user_id: number;
 }
 
 export function guard(req: Request, res: Response, next: NextFunction) {
@@ -16,10 +16,19 @@ export function guard(req: Request, res: Response, next: NextFunction) {
     return next(createHttpError(401, "Token JWT is required"));
   }
 
-  jwt.verify(tokenJWT, process.env.JWT_SECRET as string, (err, payload) => {
+  const JWT_SECRET = process.env.JWT_SECRET
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment varibales")
+  }
+  jwt.verify(tokenJWT, JWT_SECRET, (err, payload: unknown) => {
     if (err) {
       return next(createHttpError(401, "Invalid token JWT"));
     }
+
+    if (typeof payload !== "object" || payload === null || !("user_id" in payload)) {
+      return next(createHttpError(401, "Invalid token payload"));
+    }
+     
 
     req.apiUserId = (payload as JwtPayload).user_id;
     next();
