18136604b694c84e3a3735db72060696afd274eb
Hector Lozano <hectorlg87@gmail.com>
2025-09-02 18:02:19 +0200

    feat: new scripts for initalization and reset db

--- server/src/init-db.ts
+++ server/src/init-db.ts
@@ -1,5 +1,5 @@
 import { Prisma } from "@prisma/client";
-import prisma from "./config/db.js";
+import prisma from "./config/db";
 import bcrypt from "bcrypt";
 import readline, { Interface } from "readline";
 
