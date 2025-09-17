7f0691a1f485600371b657c04443ea3820eaad4b
oscar <oscar.caasmontes@gmail.com>
2025-09-17 10:44:29 +0200

    separar enviroments en 2, para resolver el mode production cuando se hace el build

--- client/src/utils/resolveBaseUrlEnv.ts
+++ client/src/utils/resolveBaseUrlEnv.ts
@@ -1,25 +1,12 @@
-type Environment = "development" | "production";
+export const resolveBaseURLFromEnv = (): string => {
+	const baseURL = import.meta.env.VITE_BASE_URL as string | undefined;
 
-export interface EnvType {
-	VITE_MODE: Environment;
-	VITE_BASE_DEV_URL?: string;
-	VITE_BASE_PROD_URL?: string;
-}
-
-export const resolveBaseURL = (env: EnvType): string => {
-	const baseURL =
-		env.VITE_MODE === "development"
-			? env.VITE_BASE_DEV_URL
-			: env.VITE_BASE_PROD_URL;
-
-	if (!baseURL)
+	if (!baseURL) {
 		throw new Error(
-			`baseURL not defined for ${env.VITE_MODE}, verify environment variables`,
+			`VITE_BASE_URL not defined for mode "${import.meta.env.MODE}". 
+       Check your .env.${import.meta.env.MODE} file.`,
 		);
+	}
 
 	return baseURL;
-};
-
-export const resolveBaseURLFromEnv = (): string => {
-	return resolveBaseURL(import.meta.env as unknown as EnvType);
 };
