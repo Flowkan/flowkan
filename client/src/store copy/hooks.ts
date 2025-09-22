be2a2202746d50b0c3d747fcc48d7a4b5637296a
Paula Barrionuevo <paulabc.1998@gmail.com>
2025-09-22 17:31:38 +0200

    commit

---
+++ client/src/store copy/hooks.ts
@@ -0,0 +1,193 @@
+import { useCallback } from "react";
+import { useAppDispatch, useAppSelector } from ".";
+import type { Column, Task } from "../pages/boards/types";
+import type { Credentials, User } from "../pages/login/types";
+import {
+	login,
+	logout,
+	resetError,
+	fetchBoards,
+	fetchBoard,
+	addBoard,
+	addColumn,
+	removeColumn,
+	addTask,
+	editTask,
+	removeTask,
+	editColumn,
+	addAssignee,
+	removeAssignee,
+	updateProfile,
+	loadedProfile,
+} from "./actions";
+import {
+	isAuthenticated,
+	getAuthError,
+	getBoards,
+	getCurrentBoard,
+	getBoardsLoading,
+	getBoardsError,
+	getUiPending,
+	getUiError,
+} from "./selectors";
+import type { ProfileType } from "../pages/profile/types";
+
+//
+// ─── AUTH HOOKS ──────────────────────────────────────────────
+//
+export function useAuth() {
+	return useAppSelector(isAuthenticated);
+}
+
+export function useAuthError() {
+	return useAppSelector(getAuthError);
+}
+
+export function useLoginAction() {
+	const dispatch = useAppDispatch();
+	return function (credentials: Credentials) {
+		return dispatch(login(credentials));
+	};
+}
+
+export function useLogoutAction() {
+	const dispatch = useAppDispatch();
+	return function () {
+		return dispatch(logout());
+	};
+}
+
+export function useUiResetError() {
+	const dispatch = useAppDispatch();
+	return function () {
+		return dispatch(resetError());
+	};
+}
+
+//
+// ─── BOARDS PROFILE ──────────────────────────────────────────────
+//
+export function useUpdatedProfile() {
+	const dispatch = useAppDispatch();
+	return function ({ user, profile }: { user: User; profile: ProfileType }) {
+		return dispatch(updateProfile({ user, profile }));
+	};
+}
+
+export function useLoadedProfile() {
+	const dispatch = useAppDispatch();
+	return function () {
+		return dispatch(loadedProfile());
+	};
+}
+
+//
+// ─── BOARDS HOOKS ──────────────────────────────────────────────
+//
+export function useBoards() {
+	return useAppSelector(getBoards);
+}
+
+export function useCurrentBoard() {
+	return useAppSelector(getCurrentBoard);
+}
+
+export function useBoardsLoading() {
+	return useAppSelector(getBoardsLoading);
+}
+
+export function useBoardsError() {
+	return useAppSelector(getBoardsError);
+}
+
+export function useFetchBoardsAction() {
+	const dispatch = useAppDispatch();
+	return function (skip: number, limit: number) {
+		return dispatch(fetchBoards(skip, limit));
+	};
+}
+
+export function useFetchBoardByIdAction() {
+	const dispatch = useAppDispatch();
+	return useCallback(
+		(id: string) => {
+			dispatch(fetchBoard(id));
+		},
+		[dispatch],
+	);
+}
+
+export function useAddBoardAction() {
+	const dispatch = useAppDispatch();
+	return function (board: FormData) {
+		return dispatch(addBoard(board));
+	};
+}
+
+export function useAddColumnAction() {
+	const dispatch = useAppDispatch();
+	return function (boardId: string, column: Column) {
+		return dispatch(addColumn(boardId, column));
+	};
+}
+
+export function useUpdateColumnAction() {
+	const dispatch = useAppDispatch();
+	return function (columnId: number, column: Partial<Column>) {
+		return dispatch(editColumn(columnId, column));
+	};
+}
+
+export function useDeleteColumnAction() {
+	const dispatch = useAppDispatch();
+	return function (columnId: string) {
+		return dispatch(removeColumn(columnId));
+	};
+}
+
+export function useAddTaskAction() {
+	const dispatch = useAppDispatch();
+	return function (columnId: number, task: Partial<Task>) {
+		return dispatch(addTask(columnId, task));
+	};
+}
+
+export function useUpdateTaskAction() {
+	const dispatch = useAppDispatch();
+	return function (columnId: number, taskId: string, task: Partial<Task>) {
+		return dispatch(editTask(columnId, taskId, task));
+	};
+}
+
+export function useDeleteTaskction() {
+	const dispatch = useAppDispatch();
+	return function (columnId: string, taskId: string) {
+		return dispatch(removeTask(columnId, taskId));
+	};
+}
+
+// ─── ASSIGNEES ────────────────────────────────
+export function useAddAssigneeAction() {
+	const dispatch = useAppDispatch();
+	return function (cardId: number, userId: number) {
+		return dispatch(addAssignee(cardId, userId));
+	};
+}
+
+export function useRemoveAssigneeAction() {
+	const dispatch = useAppDispatch();
+	return function (cardId: number, userId: number) {
+		return dispatch(removeAssignee(cardId, userId));
+	};
+}
+
+//
+// ─── UI HOOKS ──────────────────────────────────────────────
+//
+export function useUiPending() {
+	return useAppSelector(getUiPending);
+}
+
+export function useUiError() {
+	return useAppSelector(getUiError);
+}
---
+++ client/src/store copy/selectors.ts
@@ -0,0 +1,67 @@
+import type { RootState } from ".";
+
+//
+// ─── AUTH ──────────────────────────────────────────────
+//
+export const isAuthenticated = (state: RootState) => state.auth.isAuthenticated;
+
+export const getAuthError = (state: RootState) => state.auth.error;
+
+//
+// ─── PROFILE ──────────────────────────────────────────────
+//
+export const getUserLogged = (state:RootState) => state.auth.user;
+
+export const getProfile = (state:RootState) => state.profile;
+
+//
+// ─── BOARDS ──────────────────────────────────────────────
+//
+export const getBoards = (state: RootState) => state.boards.boards;
+
+export const getBoardsByTitle = (state: RootState, seachTitle: string) =>
+	state.boards.boards.filter((b) =>
+		b.title.toLowerCase().includes(seachTitle.toLowerCase()),
+	);
+
+export const getBoardByMember = (state: RootState, searchMember: string) => {
+  if (searchMember.includes("@")) {
+    return state.boards.boards.filter((b) => b.members.some(m => m.user.email.toLowerCase().includes(searchMember.toLowerCase()) ))
+  }
+  return state.boards.boards.filter((b) =>
+    b.members.some(
+      (m) =>
+        m.user.name.toLowerCase().includes(searchMember.toLowerCase()) ||
+        m.user.email.toLowerCase().includes(searchMember.toLowerCase()),
+    ),
+  );
+};
+
+export const getBoardFilterCombine = (state: RootState, searchTitle:string, searchMember:string) => {
+  if(searchTitle && searchMember) {
+    const boardTitle = getBoardsByTitle(state, searchTitle);
+    const boardMember = getBoardByMember(state, searchMember);
+
+    return boardTitle.filter((board) => 
+      boardMember.some(memberBoard => memberBoard.id === board.id)
+    );
+  } 
+  if(searchTitle) return getBoardsByTitle(state, searchTitle);
+  if(searchMember) return getBoardByMember(state, searchMember);
+  return getBoards(state);
+}
+
+export const getCurrentBoard = (state: RootState) => state.boards.currentBoard;
+
+export const getBoardsLoading = (state: RootState) => state.boards.loading;
+
+export const getBoardsError = (state: RootState) => state.boards.error;
+
+//
+// ─── UI ──────────────────────────────────────────────
+//
+export const getUi = (state: RootState) => state.ui;
+
+export const getUiPending = (state: RootState) => state.ui.pending;
+
+export const getUiError = (state: RootState) => state.ui.error;
