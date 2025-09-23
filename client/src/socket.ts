485ff677d8f179163c7223ffdaa0b616993ea88a
oscar <oscar.caasmontes@gmail.com>
2025-09-23 20:28:13 +0200

    cambio path de socket

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -3,5 +3,7 @@ import { io } from "socket.io-client";
 const URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
 
 export const socket = io(URL, {
-	autoConnect: false,
+	path: "/api/socket.io",
+	withCredentials: true,
+	transports: ["websocket"],
 });
