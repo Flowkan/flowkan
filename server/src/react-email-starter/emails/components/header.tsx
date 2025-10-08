77fe0a44651953dae267dd30a5e80e6728f34faf
Hector Lozano <hectorlg87@gmail.com>
2025-10-08 15:50:04 +0200

    feat: logo y fondo en banner de email

--- server/src/react-email-starter/emails/components/header.tsx
+++ server/src/react-email-starter/emails/components/header.tsx
@@ -1,26 +1,33 @@
-import * as React from 'react';
-import { Heading, Section, Tailwind } from "@react-email/components";
+import * as React from "react";
+import { Heading, Img, Section, Tailwind } from "@react-email/components";
 
 const HeaderEmail = () => {
-    return (   
-        <Tailwind
-        config={{
-          theme: {
-            extend: {
-              colors: {
-                brand: '#df539f', 
-              },
+  return (
+    <Tailwind
+      config={{
+        theme: {
+          extend: {
+            colors: {
+              brand: "#1e2939",
             },
           },
-        }}
-        >
-            <Section className="bg-brand text-center p-10 rounded-lg">              
-                <Heading as="h1" className="text-white text-3xl font-bold mt-4">
-                    Flowkan
-                </Heading>              
-            </Section>
-        </Tailwind>     
-    );
+        },
+      }}
+    >
+      <Section className="bg-brand text-center p-4 rounded-lg">
+        <Heading as="h1" className="text-white text-3xl font-light text-center">
+          <Img
+            src="https://flowkan.es/favicon.ico"
+            alt="Flowkan-icon"
+            width={32}
+            height={32}
+            className="inline-block align-middle"
+          />
+          <span className="inline-block align-middle">lowkan</span>
+        </Heading>
+      </Section>
+    </Tailwind>
+  );
 };
 
 export default HeaderEmail;
