0bcd97436e5952051abe7e142c5685b5471bb4fb
Hector Lozano <hectorlg87@gmail.com>
2025-10-08 13:46:58 +0200

    feat: cambio en el estilo del header y añadido imagen del logo de la web

--- server/src/react-email-starter/emails/components/header.tsx
+++ server/src/react-email-starter/emails/components/header.tsx
@@ -1,26 +1,36 @@
-import * as React from 'react';
-import { Heading, Section, Tailwind } from "@react-email/components";
+import * as React from "react";
+import { Heading, Img, Section, Tailwind } from "@react-email/components";
 
 const HeaderEmail = () => {
-    return (   
-        <Tailwind
-        config={{
-          theme: {
-            extend: {
-              colors: {
-                brand: '#df539f', 
-              },
+  return (
+    <Tailwind
+      config={{
+        theme: {
+          extend: {
+            colors: {
+              brand: "#1e2939",
             },
           },
-        }}
+        },
+      }}
+    >
+      <Section className="bg-brand flex justify-center text-center p-0 items-center rounded-lg">
+        <Heading
+          as="h1"
+          className="flex items-center justify-center text-3xl font-normal p-2 m-1"
         >
-            <Section className="bg-brand text-center p-10 rounded-lg">              
-                <Heading as="h1" className="text-white text-3xl font-bold mt-4">
-                    Flowkan
-                </Heading>              
-            </Section>
-        </Tailwind>     
-    );
+          <Img
+            src="http://localhost:3001/static/favicon.ico"
+            alt="Flowkan-icon"
+            width={32}
+            height={32}
+          />
+          <span className="text-white pt-2 pb-2 mt-2">lowkan</span>
+          {/* lowkan */}
+        </Heading>
+      </Section>
+    </Tailwind>
+  );
 };
 
 export default HeaderEmail;
